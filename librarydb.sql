2a2046064eb640f962d09036e5250d912f8100e3
Anang Kurniawan <161510163@student.budiluhur.ac.id>
2019-06-16 01:31:57 +0700

    UAS_FaisalAriefDeswar_1611510304_JavaEnterprise

---
+++ librarydb.sql
@@ -0,0 +1,43 @@
+﻿# Host: localhost  (Version 5.5.5-10.1.16-MariaDB)
+# Date: 2019-06-16 00:46:06
+# Generator: MySQL-Front 5.4  (Build 3.46)
+# Internet: http://www.mysqlfront.de/
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "categories"
+#
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name_category` varchar(30) DEFAULT NULL,
+  `code` int(11) DEFAULT NULL,
+  `information` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Structure for table "books"
+#
+
+DROP TABLE IF EXISTS `books`;
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `name_book` varchar(30) DEFAULT NULL,
+  `code_category` int(11) DEFAULT NULL,
+  `type` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKs8euxlbsy23j0t5frvj9634c5` (`code_category`),
+  CONSTRAINT `FKs8euxlbsy23j0t5frvj9634c5` FOREIGN KEY (`code_category`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Structure for table "hibernate_sequence"
+#
+
+DROP TABLE IF EXISTS `hibernate_sequence`;
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
